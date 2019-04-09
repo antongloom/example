8e0f82a8e7cd61ae3faf00a456b34e5fcf86e536
Kleshnev Anton <antongloom89@gmail.ru>
2019-04-09 22:16:34 +0300

    #21 commit

---
+++ Dmitry/ajax/ajax2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Ajax</title>
+</head>
+<body>
+
+	<form>
+		<p>Email</p>
+		<input type="email" name="email"><br>
+		<p>Телефон</p>
+		<input type="text" name="phone"><br>
+		<p>Имя</p>
+		<input type="text" name="name"><br>
+		<input type="button" id="send" value="Отправить">
+	</form>
+	<div id="result"></div>
+
+
+<script src="script.js"></script>	
+</body>
+</html>
